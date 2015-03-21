ef750db0007e11d7e3181bb2e53758bee414d974
YuboHan <hanyubo95@gmail.com>
2015-03-21 02:41:37 -0400

    yo
    yo

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 from clientpy2 import *
 from tick_info import *
-import socket
-import sys
 import sys
 import re
 
 my_profile = Profile()
-equity_array = []
+# AAPL=1, ATVI=2, EA=3, FB=4, GOOG=5, MSFT=6, SBUX=7, SNY=8, TSLA=9, TWTR=10
+equity_array = [Equity("AAPL"),
+                Equity("ATVI"),
+                Equity("EA"),
+                Equity("FB"),
+                Equity("GOOG"),
+                Equity("MSFT"),
+                Equity("XOM"),
+                Equity("SNY"),
+                Equity("TSLA"),
+                Equity("TWTR")]
 
-def initialize_equity():
-    #CURRENT EQUITIES
-    match  = re.findall('(\w+) \d+', qrun("MY_SECURITIES") )
-    for equity_name in match:
-        equity_array.append(Equity(equity_name))
-def qrun(*command): #quick run
-    ret = run("CounterLogic_EMY", "yubodoxical", *command)
+def qrun(*commands):
+    ret = run("CounterLogic_EMY", "yubodoxical", *commands)
     return ret
 
 def initialize_ticker(profile=my_profile): #runs at the start of each tick to get values for everything
+
     #MY_CASH
-    match  = re.search('MY_CASH_OUT (\d+)', qrun("MY_CASH") )
+    match = re.search('MY_CASH_OUT (\d+)', run("CounterLogic_EMY", "yubodoxical", "MY_CASH") )
     my_profile.money = float(match.group(1))
 
-    if equity_array.len == 0:
-        initialize_equity()
-
     #MY_SECURITIES
-    output_string = qrun("MY_SECURITIES")
+    output_string = run("CounterLogic_EMY", "yubodoxical", "MY_SECURITIES")
     for equity in equity_array:
         match = re.search(equity.name + ' (\S+)', output_string)
         equity.my_owned = float(match.group(1))
@@ -34,48 +35,57 @@ def initialize_ticker(profile=my_profile): #runs at the start of each tick to ge
         equity.my_dividend_ratio = float(match.group(1))
 
     #MY_ORDERS
-    output_string = qrun("MY_ORDERS")
+    output_string = run("CounterLogic_EMY", "yubodoxical", "MY_ORDERS")
     for equity in equity_array:
         match = re.search('BID ' + equity.name + ' (\S+)', output_string)
-        
+        if match:
+            equity.my_bid = float(match.group(1))
+        else:
+            equity.my_bid = 0
+        match = re.search('ASK ' + equity.name + ' \S+ (\S+)', output_string)
+        if match:
+            equity.my_ask = float(match.group(1))
+        else:
+            equity.my_ask = 0
 
-    qrun('BID EA 1 2')
-    qrun('ASK EA 1000 1')
-    output_string = qrun("MY_ORDERS")
-    #print output_string
-    
-    
     #ORDERS
     for equity in equity_array:
-        output_string = qrun("ORDERS " + equity.name)
+        output_string = run("CounterLogic_EMY", "yubodoxical", "ORDERS " + equity.name)
+        
+        #bid price
         match = re.findall('BID ' + equity.name + ' (\S+)', output_string)
-        float_match=[]
+        float_match = []
         for x in match:
             y = float(x)
             float_match.append(y)
         equity.bid_price = float_match
+
+        #bid shares
         match = re.findall('BID ' + equity.name + ' \S+ (\S+)', output_string)
-        float_match=[]
+        float_match = []
         for x in match:
             y = float(x)
             float_match.append(y)
         equity.bid_shares = float_match
 
-        match = re.finall('ASK ' + equity.name + ' (\S+)', output_string)
-        float_match=[]
+        #ask price
+        match = re.findall('ASK ' + equity.name + ' (\S+)', output_string)
+        float_match = []
         for x in match:
             y = float(x)
             float_match.append(y)
         equity.ask_price = float_match
-        match = re.finall('ASK ' + equity.name + ' \S+ (\S+)', output_string)
-        float_match=[]
+
+        #ask shares
+        match = re.findall('ASK ' + equity.name + ' \S+ (\S+)', output_string)
+        float_match = []
         for x in match:
             y = float(x)
             float_match.append(y)
         equity.ask_shares = float_match
 
     #SECURITIES
-    output_string = qrun("SECURITIES")
+    output_string = run("CounterLogic_EMY", "yubodoxical", "SECURITIES")
     for equity in equity_array:
         match = re.search(equity.name + ' (\S+)', output_string)
         equity.net_worth = float(match.group(1))
